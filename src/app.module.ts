d625b379e1814f20849348cb9e0ee1f4a88e4975
Tho Dang <tho.anguc@ohmidasvn.com>
2024-07-25 09:20:02 +0700

    feat: add orders module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
